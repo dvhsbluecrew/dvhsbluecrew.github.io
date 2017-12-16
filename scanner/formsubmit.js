e37f7250127eb1288932baf8ae777f66840e1cad
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2017-12-16 00:20:43 -0800

    first working build

--- scanner/formsubmit.js
+++ scanner/formsubmit.js
@@ -22,10 +22,52 @@ function formsubmit() {
 
 	$.ajax(settings).done(function (response) {
 	  console.log(response);
-	  var results = JSON.parse(response);
+	  modaltitle.innerHTML = response.name + ' (' + response.idnum + ')';
+	  pagetitle.innerHTML = response.name + ' (' + response.idnum + ')';
+
+	  if(response.error == 0) {
+	  	valid.innerHTML = '<div class="d-inline bg-success">Success! You checked the student in at ' + response.checkintime + '.</div>';
+
+	  	if(response.drinkpass > 0) {
+	  		drinkpass.innerHTML = 'Student has ' + response.drinkpass + ' drink pass(es).';
+	  	}
+	  	if(response.guestpass > 0) {
+	  		guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
+	  	}
+	  }
+	  else if(response.error == 2) {
+	  	valid.innerHTML = '<div class="d-inline bg-warning">This student was already checked in by ' + response.checkinstaff + ' at ' + response.checkintime + '.</div>';
+
+	  	if(response.drinkpass > 0) {
+	  		drinkpass.innerHTML = 'Student has ' + response.drinkpass + ' drink pass(es).';
+	  	}
+	  	if(response.guestpass > 0) {
+	  		guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
+	  	}
+	  }
+	  else if(response.error == 3){
+	  	valid.innerHTML = '<div class="d-inline bg-danger">We were unable to find a valid ticket with student ID #' + response.idnum + '.</div>';
+	  	drinkpass.innerHTML = 'Try scanning the card again. If the problem persists, ask Ivy Wang for help.';
+	  }
+	  else {
+	  	valid.innerHTML = '<div class="d-inline bg-danger">The authentication token you provided was invalid.</div>';
+	  	drinkpass.innerHTML = 'Try logging out and logging in again. Note that each account can only be signed into one device at a time.';
+	  }
 	});
 
 	$("#myModal").modal();
+
+	var modaltitle = document.getElementById('modalTitle');
+	var pagetitle = document.getElementById('title');
+	var valid = document.getElementById('valid');
+	var drinkpass = document.getElementById('drinkpass');
+	var guestpass = document.getElementById('guestpass');
+
+	modaltitle.innerHTML = 'Please Wait...';
+	pagetitle.innerHTML = 'Please Wait...';
+	valid.innerHTML = '';
+	drinkpass.innerHTML = '';
+	guestpass.innerHTML = '';
 
 	return false;
 }
