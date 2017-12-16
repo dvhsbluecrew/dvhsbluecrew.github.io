a6ac6cd375247c851bbca577ecbdc726a3e73405
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2017-12-16 01:12:17 -0800

    improvements to login

--- scanner/formsubmit.js
+++ scanner/formsubmit.js
@@ -1,4 +1,4 @@
-//Get parameters from URL
+//Get parameters from URL function
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -9,9 +9,52 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+//Startup script
+var username = document.getElementById('dropdown02');
+var signinlink = document.getElementById('signinlink');
+var modaltitle = document.getElementById('modalTitle');
+var pagetitle = document.getElementById('title');
+var valid = document.getElementById('valid');
+var drinkpass = document.getElementById('drinkpass');
+var guestpass = document.getElementById('guestpass');
+
 var token = getParameterByName('token');
 var tokenfield = document.getElementById('token');
 tokenfield.value = token;
+
+//Initial check to see if token is valid
+var urlstring = "https://script.google.com/macros/s/AKfycbzxPD0XVTHnUWMctHFjPiEzwnSX2CrFhtOqQux_6mAFT4cmbdsh/exec?token=" + token;
+
+	var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": urlstring,
+	  "method": "GET"
+	}
+
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+
+	  if(response.error == 1) {
+		modaltitle.innerHTML = 'Invalid token';
+	  	pagetitle.innerHTML = 'Invalid Login';
+
+	  	valid.innerHTML = '<div class="d-inline bg-danger">Invalid login.</div>';
+	  	drinkpass.innerHTML = 'Please log in again. Note that each account can only be signed into one device at a time.';
+
+	  	$("#myModal").modal();
+	  }
+	  else {
+	  	username.innerHTML = response.checkinstaff;
+	  	signinlink.innerHTML = 'Log Out';
+
+	  	if(response.dashboard == 1) {
+		  	var $node = null;
+	        $node = $('<a class="dropdown-item" href="#">Open Dashboard</a>');
+	        $("#userdropdown").prepend($node);
+    	}
+	  }
+	});
 
 //Form submit
 $(function() { //shorthand document.ready function
@@ -71,18 +114,16 @@ function formsubmit() {
 	  	drinkpass.innerHTML = 'Try scanning the card again. If the problem persists, ask Ivy Wang for help.';
 	  }
 	  else {
+	  	modaltitle.innerHTML = 'Invalid token';
+	  	pagetitle.innerHTML = 'Invalid Login';
 	  	valid.innerHTML = '<div class="d-inline bg-danger">Invalid login.</div>';
-	  	drinkpass.innerHTML = 'Try logging out and logging in again. Note that each account can only be signed into one device at a time.';
+	  	drinkpass.innerHTML = 'Please log in again. Note that each account can only be signed into one device at a time.';
+	  	username.innerHTML = 'Not Signed In';
+	  	signinlink.innerHTML = 'Sign In';
 	  }
 	});
 
 	$("#myModal").modal();
-
-	var modaltitle = document.getElementById('modalTitle');
-	var pagetitle = document.getElementById('title');
-	var valid = document.getElementById('valid');
-	var drinkpass = document.getElementById('drinkpass');
-	var guestpass = document.getElementById('guestpass');
 
 	modaltitle.innerHTML = 'Please Wait...';
 	pagetitle.innerHTML = 'Please Wait...';
