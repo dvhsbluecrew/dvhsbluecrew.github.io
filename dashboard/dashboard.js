35e2c62cceff277450d9dbf72001057a3e7f57e9
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2018-01-12 16:54:05 -0800

    Add check in script to dashboard

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -108,6 +108,80 @@ function signout() {
   });
 }
 
+//Form submit
+$(function() { //shorthand document.ready function
+    $('#searchID').on('submit', function(e) { //use on if jQuery 1.7+
+        e.preventDefault();  //prevent form from submitting
+        formsubmit();
+    });
+});
+
+function formsubmit() {
+  //Get values from form
+  var idnumber = document.getElementById('search').value;
+  var token = document.getElementById('token').value;
+
+  var urlstring = "https://script.google.com/macros/s/AKfycbzxPD0XVTHnUWMctHFjPiEzwnSX2CrFhtOqQux_6mAFT4cmbdsh/exec?id=" + idnumber + "&token=" + token;
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": urlstring,
+    "method": "GET"
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    console.log(response.drinkpass2);
+    modaltitle.innerHTML = response.name + ' (' + response.idnum + ')';
+    pagetitle.innerHTML = response.name + ' (' + response.idnum + ')';
+
+    if(response.error == 0) {
+      valid.innerHTML = '<div class="d-inline bg-success">Success! You checked the student in at ' + response.checkintime + '.</div>';
+
+      if(response.drinkpass2 == 1) {
+        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink pass.';
+      }
+      else if(response.drinkpass2 > 1) {
+        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink passes.';
+      }
+      if(response.guestpass > 0) {
+        guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
+      }
+    }
+    else if(response.error == 2) {
+      valid.innerHTML = '<div class="d-inline bg-warning">This student was checked in by ' + response.checkinstaff + ' at ' + response.checkintime + '.</div>';
+
+      if(response.drinkpass2 == 1) {
+        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink pass.';
+      }
+      else if(response.drinkpass2 > 1) {
+        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink passes.';
+      }
+      if(response.guestpass > 0) {
+        guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
+      }
+    }
+    else if(response.error == 3){
+      valid.innerHTML = '<div class="d-inline bg-danger">We were unable to find a valid ticket with student ID #' + response.idnum + '.</div>';
+      drinkpass.innerHTML = 'Try scanning the card again. If the problem persists, ask Ivy Wang for help.';
+    }
+    else {
+      notloggedin();
+    }
+  });
+
+  $("#myModal").modal();
+
+  modaltitle.innerHTML = 'Please Wait...';
+  pagetitle.innerHTML = 'Please Wait...';
+  valid.innerHTML = '';
+  drinkpass.innerHTML = '';
+  guestpass.innerHTML = '';
+
+  return false;
+}
+
 //Table Sort Function
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
