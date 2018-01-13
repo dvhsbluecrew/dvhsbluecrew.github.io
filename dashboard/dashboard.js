cf8cb21ef00dc9ee95fa4557d23f6f92a424b6b2
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2018-01-12 18:01:21 -0800

    Remove unused table search function

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -278,37 +278,3 @@ function sortTable(n) {
     }
   }
 }
-
-//Table Search Bar Function
-function searchBar() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("tablesearch");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("tableresults");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } 
-      else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
-
-//Get parameters from URL function
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
